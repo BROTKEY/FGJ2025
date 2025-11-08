80c6c2c82da056381f1eba25ef91906778592b20
Jonas Küper <jonaskueper@gmx.de>
2025-11-08 23:23:01 +0100

    Microgames: Added interface BaseMicrogame

--- Game/Scripts/Microgames/FryMeUp/fry_me_up.gd
+++ Game/Scripts/Microgames/FryMeUp/fry_me_up.gd
@@ -1,10 +1,10 @@
-extends Node
+extends BaseMicrogame
 
 var cooked_chicken = preload("res://Assets/Microgames/FryMeUp/Cooked_Chicken_JE3_BE3.png")
 var cooking_percentage = 0
 var won = false
 @export var cooking_time_s = 1
-@export var difficulty = 1
+@export var difficulty = 1.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:     
@@ -15,8 +15,16 @@ func _process(delta: float) -> void:
 	else:
 		$Fire.hide()
 		
-		
 	if cooking_percentage >= 1 and !won:
 		$Chicken.texture = cooked_chicken
 		$Particels.emitting = true 
 		won = true
+
+func get_difficulty() -> float:
+	return difficulty
+
+func set_difficulty(value: float) -> void:
+	difficulty = value
+
+func get_won() -> bool:
+	return won
---
+++ Game/Scripts/Microgames/base_microgame.gd
@@ -0,0 +1,14 @@
+class_name BaseMicrogame
+extends Node
+
+
+func get_won() -> bool:
+	printerr("Not Implemented!")
+	return false
+
+func get_difficulty() -> float:
+	printerr("Not Implemented!")
+	return 1.0
+
+func set_difficulty(_value: float) -> void:
+	printerr("Not Implemented!")
