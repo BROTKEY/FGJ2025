fa55cca98f1e915246163b17cb84e1aadf57e170
Jonas Küper <jonaskueper@gmx.de>
2025-11-08 15:06:09 +0100

    WIP: Restructuring SceneManager

--- Game/Scripts/GlobalScripts/scene_manager.gd
+++ Game/Scripts/GlobalScripts/scene_manager.gd
@@ -3,7 +3,7 @@ extends Node
 
 enum SceneType {
 	MAIN_MENU,
-	MICRO_GAME,
+	SCENE,
 }
 
 
@@ -24,11 +24,31 @@ static func _set_only_child(parent: Node, new_child: Node):
 		parent.add_child(new_child)
 
 
-func show_microgame(ugame: Node):
-	_set_only_child(_current_scene_node, ugame)
-	get_tree().current_scene = ugame
+func show_scene(scene: Node) -> Node:
+	_set_only_child(_current_scene_node, scene)
+	get_tree().current_scene = scene
 	_main_menu.hide()
-	_active_scene_type = SceneType.MICRO_GAME
+	_active_scene_type = SceneType.SCENE
+	return scene
+
+
+func show_split_screen(left_scene: Node, right_scene: Node):
+	var split_screen = SplitScreen.create_split_screen(left_scene, right_scene)
+	return show_scene(split_screen)
+
+
+func show_split_screen_packed(left_scene: PackedScene, right_scene: PackedScene):
+	return show_split_screen(
+		left_scene.instantiate(),
+		right_scene.instantiate()
+	)
+
+
+func show_split_screen_from_file(left_scene: String, right_scene: String):
+	return show_split_screen(
+		load(left_scene).instantiate(),
+		load(right_scene).instantiate()
+	)
 
 
 func show_main_menu():
