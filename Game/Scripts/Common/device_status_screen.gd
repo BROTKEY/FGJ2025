66200ce8828c6c83bc1ace08d38b854d521d8c49
Jonas Küper <jonaskueper@gmx.de>
2025-11-09 16:08:01 +0100

    updated indicators

--- Game/Scripts/Common/device_status_screen.gd
+++ Game/Scripts/Common/device_status_screen.gd
@@ -18,15 +18,18 @@ const midi_trans = preload("res://Assets/Common/MidiKeyboardTrans.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if InputManager.is_device_connected(InputManager.InputDevice.PINECIL):
-		$IconsContainer/Pinecil.texture = pinecil
-	else:
-		$IconsContainer/Pinecil.texture = pinecil_trans
-	if InputManager.is_device_connected(InputManager.InputDevice.MIDI_KEYBOARD):
-		$IconsContainer/MidiKeyboard.texture = midi
-	else:
-		$IconsContainer/MidiKeyboard.texture = midi_trans
-	if InputManager.is_device_connected(InputManager.InputDevice.WII_BOARD):
-		$IconsContainer/WiiBoard.texture = wiiboard
-	else:
-		$IconsContainer/WiiBoard.texture = wiiboard_trans
+	$IconsContainer/Pinecil.visible = InputManager.is_device_connected(InputManager.InputDevice.PINECIL)
+	$IconsContainer/MidiKeyboard.visible = InputManager.is_device_connected(InputManager.InputDevice.MIDI_KEYBOARD)
+	$IconsContainer/WiiBoard.visible = InputManager.is_device_connected(InputManager.InputDevice.WII_BOARD)
+	#if InputManager.is_device_connected(InputManager.InputDevice.PINECIL):
+		#$IconsContainer/Pinecil.texture = pinecil
+	#else:
+		#$IconsContainer/Pinecil.texture = pinecil_trans
+	#if InputManager.is_device_connected(InputManager.InputDevice.MIDI_KEYBOARD):
+		#$IconsContainer/MidiKeyboard.texture = midi
+	#else:
+		#$IconsContainer/MidiKeyboard.texture = midi_trans
+	#if InputManager.is_device_connected(InputManager.InputDevice.WII_BOARD):
+		#$IconsContainer/WiiBoard.texture = wiiboard
+	#else:
+		#$IconsContainer/WiiBoard.texture = wiiboard_trans
