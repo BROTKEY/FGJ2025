7b9bcd5623691b2357d2bd0887a11e9e6874a3b0
anonym6000 <github@anonym6000.de>
2025-11-09 14:29:03 +0100

    Added boss music and fixed menu music loop

--- Game/Scripts/GlobalScripts/audio_manager.gd
+++ Game/Scripts/GlobalScripts/audio_manager.gd
@@ -11,7 +11,7 @@ var music_game_jump: Resource = preload("uid://booiouw0b5xdu")
 var music_game_keyboard_reflex: Resource = preload("uid://v5hd8s7afyir")
 var music_game_radio_dial: Resource = preload("uid://c0y8p48yrthkr")
 var music_game_shake_me: Resource = preload("uid://cfc81505ijidp")
-var music_game_team_platformer: Resource = null
+var music_game_boss: Resource = preload("uid://b2gr377vycj7b")
 
 var music_transition_base: Resource = preload("uid://ie4vdvs8a5mw")
 var music_transition_fry_me_up: Resource = preload("uid://epafrvdupsjc")
@@ -19,7 +19,6 @@ var music_transition_jump: Resource = preload("uid://tybtxr35u4ax")
 var music_transition_keyboard_reflex: Resource = preload("uid://dq7jlaw28wu6x")
 var music_transition_radio_dial: Resource = preload("uid://cx7koyhccur8x")
 var music_transition_shake_me: Resource = preload("uid://bpfdalei8lr6m")
-var music_transition_team_platformer: Resource = null
 
 var audio_microgames_games: Dictionary[String, Resource] = {
 	"FryMeUp": music_game_fry_me_up,
@@ -69,3 +68,15 @@ func play_transition(game_name_left: String, game_name_right: String) -> void:
 	audio_player_game_base.play()
 	audio_player_game_left.play()
 	audio_player_game_right.play()
+
+func play_game_boss() -> void:
+	print("Starting boss music")
+	stop_music()
+	audio_player_game_base.stream = music_game_boss
+	audio_player_game_base.play()
+	
+func play_transition_boss() -> void:
+	print("Starting transition boss music")
+	stop_music()
+	audio_player_game_base.stream = music_transition_base
+	audio_player_game_base.play()
