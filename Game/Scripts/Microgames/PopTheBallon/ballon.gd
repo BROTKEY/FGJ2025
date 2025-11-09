a724208d1d1fca3f0d978dc6d8bab91ed5a2f7f8
Brotkey <54113864+BROTKEY@users.noreply.github.com>
2025-11-09 15:39:50 +0100

    stuff and so

--- Game/Scripts/Microgames/PopTheBallon/ballon.gd
+++ Game/Scripts/Microgames/PopTheBallon/ballon.gd
@@ -1,15 +1,10 @@
 extends Area2D
 
+signal popped
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_player_body_entered(_body: Node2D) -> void:
 	pass
 
 
-func _on_player_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_player_area_entered(area: Area2D) -> void:
+	popped.emit()
--- Game/Scripts/Microgames/PopTheBallon/pop_the_ballon.gd
+++ Game/Scripts/Microgames/PopTheBallon/pop_the_ballon.gd
@@ -5,13 +5,21 @@ var difficulty = 1
  
 var org_play_pos = Vector2(0,0)
 
+func ballon_poped() -> void:
+	if !won:
+		$Ballon/Particels.emitting=true
+		$Ballon/ColorRect.hide()
+	won=true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Ballon.rotation = randf_range(0,3.14)
 	org_play_pos = $Player.position
+	$Ballon.popped.connect(ballon_poped)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var posmod = InputManager.get_wii_center_of_mass()
+	var posmod = InputManager.get_wii_center_of_mass()*2
 	$Player.position = Vector2(org_play_pos.x + posmod.x, org_play_pos.y + posmod.y)
 
 func get_difficulty() -> float:
