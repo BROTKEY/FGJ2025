f5de296eaef3773d67576f4b4ffe169bb1c64ac2
Hwawooook <46605503+MiningXL@users.noreply.github.com>
2025-11-09 16:40:51 +0100

    fixed debugger

--- Game/Scripts/Debug/pinecil_debug.gd
+++ Game/Scripts/Debug/pinecil_debug.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var Pinecil = InputManager.get_node("Pinecil")
-const PinecilMenus = preload("res://Scripts/Types/pinecil_types.gd").PinecilMenus
+const PinecilMenus = preload("res://Scripts/Common/pinecil_types.gd").PinecilMenus
 
 func _process(_delta: float) -> void:
 	if (Time.get_ticks_msec() % 100) != 0:
@@ -21,7 +21,7 @@ func _on_main_menu_pressed() -> void:
 	InputManager.set_soldering_iron_screen(PinecilMenus.GameJamHome)
 
 func _on_temp_adjust_pressed() -> void:
-	InputManager.set_soldering_iron_screen(PinecilMenus.GameJamTemperatureAdjist)
+	InputManager.set_soldering_iron_screen(PinecilMenus.GameJamTemperatureAdjust)
 
 func _on_shake_menu_pressed() -> void:
 	InputManager.set_soldering_iron_screen(PinecilMenus.GameJamShake)
