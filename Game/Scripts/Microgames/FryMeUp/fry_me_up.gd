d87f4ef5e4ecfb3c01aba4a525b141bbbd6c8a9f
Hwawooook <46605503+MiningXL@users.noreply.github.com>
2025-11-14 21:20:48 +0100

    fixed up the FryMeUp Microgame a bit; started the splitscreen rendering overhaul

--- Game/Scripts/Microgames/FryMeUp/fry_me_up.gd
+++ Game/Scripts/Microgames/FryMeUp/fry_me_up.gd
@@ -10,6 +10,13 @@ var won = false
 var next_sample_time = 0
 var next_screen_check = 0
 
+@onready
+var FireSprite = $Background/SpriteAnchor/FireSprite
+@onready
+var ChickenSprite = $Background/SpriteAnchor/ChickenSprite
+@onready
+var ChickenParticles = $Background/SpriteAnchor/ChickenSprite/Particles
+
 func _ready() -> void:
 	InputManager.set_soldering_iron_screen(InputManager.PinecilMenus.GameJamTemperatureAdjust)
 	var random_start_temp = randi_range(8,12)*10
@@ -21,19 +28,21 @@ func _process(delta: float) -> void:
 		if (Time.get_ticks_msec() >= next_screen_check) and false: # Lags too much
 			check_and_set_pinecil_screen()
 			next_screen_check = Time.get_ticks_msec() + 500 # Check if the Pinecil is on the correct Screen every 500ms
-			
+		
+		var fire_target_animation = "fire_not_burning"
 		if InputManager.get_soldering_iron_temprature() >= 180:
 			if !won:
 				cooking_percentage += delta / cooking_time_s * difficulty
-			$Fire.show()
-		else:
-			$Fire.hide()
+			fire_target_animation = "fire_burning"
+		
+		if FireSprite.animation != fire_target_animation:
+			FireSprite.play(fire_target_animation)
 		
 		next_sample_time = Time.get_ticks_msec() + (1000/pinecil_sample_rate_hz)
 			
 	if cooking_percentage >= 1 and !won:
-		$Chicken.texture = cooked_chicken
-		$Particels.emitting = true 
+		ChickenSprite.texture = cooked_chicken
+		ChickenParticles.emitting = true 
 		won = true
 
 func check_and_set_pinecil_screen():
